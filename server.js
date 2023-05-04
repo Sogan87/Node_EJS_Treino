671e6011639be466e887e2bb61bac3ec3e235339
Sogan87 <sandro.leonardo.ti@gmail.com>
2023-05-04 11:06:36 -0300

    poreach no server

--- server.js
+++ server.js
@@ -4,7 +4,35 @@ const app = express();
 app.set("view engine","ejs")
 
 app.get("/", function(req, res){
-    res.render("pages/index");
+    const items = [
+        {   
+            title: "D ", 
+            message: "Desenvolver aplicativos"
+        },
+        {
+            title: "E ",
+            message: "EJS usa JavaScript"
+        },
+        {
+            title: "M ",
+            message: "Muito Facil"
+        },
+        {
+            title: "A ",
+            message: "Amor pelo Javascript"
+        },
+        {
+            title: "I ",
+            message: "Install EJS"
+        },
+        {
+            title: "S ",
+            message: "Sintax simples"
+        }
+    ];
+    res.render("pages/index",{
+        qualitys: items,
+    });
 })
 
 app.get("/sobre", function(req, res){
